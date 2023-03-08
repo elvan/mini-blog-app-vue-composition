00e31a35a2a2e7815bd26ef6b9a8f619d922c781
Elvan Hidayat <elvanhd@gmail.com>
2023-03-09 00:07:20 +0700

    fetching data in the setup function

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,25 +1,19 @@
 <template>
   <div class="post-list">
     <div v-for="post in posts" :key="post.id">
-      <!-- <h3>{{ post.title }}</h3> -->
       <SinglePost :post="post" />
     </div>
   </div>
 </template>
 
 <script>
-import { onMounted, onUnmounted, onUpdated } from 'vue';
 // component imports
 import SinglePost from './SinglePost.vue';
 
 export default {
   props: ['posts'],
   components: { SinglePost },
-  setup(props) {
-    onMounted(() => console.log('component mounted'));
-    onUnmounted(() => console.log('component unmounted'));
-    onUpdated(() => console.log('component updated'));
-  },
+  setup() {},
 };
 </script>
 
