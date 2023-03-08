983f73838b80bf33b52e0c9a5762d057c08c131c
Elvan Hidayat <elvanhd@gmail.com>
2023-03-08 23:36:08 +0700

    lifecycle hooks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts="posts" />
+    <PostList v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
   </div>
 </template>
 
@@ -23,8 +25,9 @@ export default {
       },
       { title: 'top 5 CSS tips', body: 'lorem ipsum', id: 2 },
     ]);
+    const showPosts = ref(true);
 
-    return { posts };
+    return { posts, showPosts };
   },
 };
 </script>
