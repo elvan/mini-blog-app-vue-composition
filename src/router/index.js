6d551d6aad8b8f96bb55f978a15f536d5f38dc8b
Elvan Hidayat <elvanhd@gmail.com>
2023-03-09 03:34:11 +0700

    composable for post details

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Details from '../views/Details.vue';
 import Home from '../views/Home.vue';
 
 const routes = [
@@ -6,6 +7,12 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/posts/:id',
+    name: 'Details',
+    component: Details,
+    props: true,
   },
 ];
 
